5ab18fd9e25e311c1da1deffe03e8719fc42690c
macdev <asky78@gmail.com>
2019-01-11 17:08:44 +0900

    등록.

---
+++ src/main/java/org/macdev/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package org.macdev.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World !!";
+    }
+}
